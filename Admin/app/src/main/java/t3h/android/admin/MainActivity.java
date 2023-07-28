60ad8510569fbc93dd9478195e04ee32883c4b1f
dkifashion <dkiwomanfashion@gmail.com>
2023-07-28 16:27:52 +0700

    handle back pressed event

--- Admin/app/src/main/java/t3h/android/admin/MainActivity.java
+++ Admin/app/src/main/java/t3h/android/admin/MainActivity.java
@@ -1,14 +1,37 @@
 package t3h.android.admin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Objects;
+
+import t3h.android.admin.listener.OnBackPressedListener;
+import t3h.android.admin.ui.DashboardFragment;
+import t3h.android.admin.ui.SignInFragment;
+
+public class MainActivity extends AppCompatActivity implements OnBackPressedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onFragmentBackPressed() {
+        finishAffinity();
+    }
+
+    @Override
+    public void onBackPressed() {
+        Fragment navHostFragment = Objects.requireNonNull(getSupportFragmentManager().findFragmentById(R.id.navHostFragment));
+        Fragment currentFragment = navHostFragment.getChildFragmentManager().getFragments().get(0);
+        if (currentFragment instanceof DashboardFragment || currentFragment instanceof SignInFragment) {
+            ((OnBackPressedListener) currentFragment).onFragmentBackPressed();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
--- Admin/app/src/main/java/t3h/android/admin/ui/DashboardFragment.java
+++ Admin/app/src/main/java/t3h/android/admin/ui/DashboardFragment.java
@@ -4,17 +4,36 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import t3h.android.admin.R;
+import t3h.android.admin.helper.AppConstant;
+import t3h.android.admin.listener.OnBackPressedListener;
 
-public class DashboardFragment extends Fragment {
+public class DashboardFragment extends Fragment implements OnBackPressedListener {
+    private boolean backPressedOnce = false;
+    private Toast toast;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dashboard, container, false);
+    }
+
+    @Override
+    public void onFragmentBackPressed() {
+        if (backPressedOnce) {
+            requireActivity().finishAffinity();
+            toast.cancel();
+        } else {
+            toast = Toast.makeText(requireActivity(), AppConstant.PRESS_AGAIN, Toast.LENGTH_SHORT);
+            toast.show();
+            backPressedOnce = true;
+            new Handler().postDelayed(() -> backPressedOnce = false, 2000);
+        }
     }
 }
