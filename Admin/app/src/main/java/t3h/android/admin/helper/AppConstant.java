744f8cd4fc18139161f041f7c881e67f548642d9
dkifashion <dkiwomanfashion@gmail.com>
2023-07-28 22:02:14 +0700

    add new constants

--- Admin/app/src/main/java/t3h/android/admin/helper/AppConstant.java
+++ Admin/app/src/main/java/t3h/android/admin/helper/AppConstant.java
@@ -2,6 +2,18 @@ package t3h.android.admin.helper;
 
 public class AppConstant {
     public static final String EMPTY_ERROR = "Please complete all information";
-    public static final String SIGN_FAILED = "Sign in failed. Please try again!";
+    public static final String SIGN_FAILED = "Sign in failed. Please try again";
     public static final String PRESS_AGAIN = "Press back again to exit";
+    public static final String DASHBOARD = "Dashboard";
+    public static final String CREATE_TOPIC = "Create topic";
+    public static final String UPDATE_TOPIC = "Update topic";
+    public static final String CREATE_AUDIO = "Create audio";
+    public static final String UPDATE_AUDIO = "Update audio";
+    public static final String EMAIL_MUST_NOT_EMPTY = "Please enter the email you registered your account with";
+    public static final String CHECK_EMAIL_MESS = "Check your email, please!";
+
+    public static final String RESET_PWD_FAILED = "Reset password failed. Please try again";
+    public static final String CONFIRM_PWD_NOT_MATCH = "Confirm password does not match new password";
+    public static final String CHANGE_PWD_SUCCESS = "Change password successfully! Please sign in again";
+    public static final String CHANGE_PWD_FAILED = "Change password failed. Please try again";
 }
