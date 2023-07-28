a961e3d180f2df831ab004a898b8b02ecad5fe1f
dkifashion <dkiwomanfashion@gmail.com>
2023-07-28 16:32:43 +0700

    build an interface to handle the application exit feature after pressing the back button twice

--- Admin/app/src/main/java/t3h/android/admin/listener/OnBackPressedListener.java
+++ Admin/app/src/main/java/t3h/android/admin/listener/OnBackPressedListener.java
@@ -1,2 +1,5 @@
-package t3h.android.admin.listener;public interface OnBackPressedListener {
+package t3h.android.admin.listener;
+
+public interface OnBackPressedListener {
+    void onFragmentBackPressed();
 }
