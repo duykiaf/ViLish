d1874d9a6f776df6343b0b4be8f97092c5d0e95c
dkifashion <dkiwomanfashion@gmail.com>
2023-07-29 14:21:31 +0700

    create constructor methods

--- Admin/app/src/main/java/t3h/android/admin/ui/ListFragment.java
+++ Admin/app/src/main/java/t3h/android/admin/ui/ListFragment.java
@@ -1,20 +1,47 @@
 package t3h.android.admin.ui;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import t3h.android.admin.R;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import t3h.android.admin.databinding.FragmentListBinding;
 
 public class ListFragment extends Fragment {
+    private FragmentListBinding binding;
+    private int position;
+
+    public ListFragment() {
+    }
+
+    public ListFragment(int position) {
+        this.position = position;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        binding = FragmentListBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
