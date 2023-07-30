1a408c9cb84a39f2e5591b505fa299f6f267bc81
dkifashion <dkiwomanfashion@gmail.com>
2023-07-30 10:54:08 +0700

    build topic and audio model

--- Admin/app/src/main/java/t3h/android/admin/model/Audio.java
+++ Admin/app/src/main/java/t3h/android/admin/model/Audio.java
@@ -1,2 +1,99 @@
-package t3h.android.admin.model;public class Audio {
+package t3h.android.admin.model;
+
+import androidx.annotation.Nullable;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class Audio {
+    private String id;
+    private String name;
+    private String audioFileFromFirebase;
+    private String audioFileFromDevice;
+    private String lyrics;
+    private String translations;
+    private int status;
+
+    public Audio() {
+    }
+
+    public Audio(String id, String name, String audioFileFromFirebase, String audioFileFromDevice, String lyrics, String translations, int status) {
+        this.id = id;
+        this.name = name;
+        this.audioFileFromFirebase = audioFileFromFirebase;
+        this.audioFileFromDevice = audioFileFromDevice;
+        this.lyrics = lyrics;
+        this.translations = translations;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAudioFileFromFirebase() {
+        return audioFileFromFirebase;
+    }
+
+    public void setAudioFileFromFirebase(String audioFileFromFirebase) {
+        this.audioFileFromFirebase = audioFileFromFirebase;
+    }
+
+    public String getAudioFileFromDevice() {
+        return audioFileFromDevice;
+    }
+
+    public void setAudioFileFromDevice(String audioFileFromDevice) {
+        this.audioFileFromDevice = audioFileFromDevice;
+    }
+
+    public String getLyrics() {
+        return lyrics;
+    }
+
+    public void setLyrics(String lyrics) {
+        this.lyrics = lyrics;
+    }
+
+    public String getTranslations() {
+        return translations;
+    }
+
+    public void setTranslations(String translations) {
+        this.translations = translations;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Audio audio = (Audio) obj;
+        return name.equalsIgnoreCase(audio.getName()) && status == audio.getStatus();
+    }
 }
--- Admin/app/src/main/java/t3h/android/admin/model/Topic.java
+++ Admin/app/src/main/java/t3h/android/admin/model/Topic.java
@@ -1,2 +1,79 @@
-package t3h.android.admin.model;public class Topic {
+package t3h.android.admin.model;
+
+import androidx.annotation.Nullable;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class Topic {
+    private String id;
+    private String name;
+    private String imagePath;
+    private int status;
+
+    public Topic() {
+    }
+
+    public Topic(String id, String name, String imagePath, int status) {
+        this.id = id;
+        this.name = name;
+        this.imagePath = imagePath;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Topic topic = (Topic) obj;
+        return name.equalsIgnoreCase(topic.getName()) && status == topic.status;
+    }
+
+    @Override
+    public String toString() {
+        return "Topic{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", imagePath='" + imagePath + '\'' +
+                ", status=" + status +
+                '}';
+    }
 }
