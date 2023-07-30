f03b2926152c236c10daa28ee362dd8750a3ae7a
dkifashion <dkiwomanfashion@gmail.com>
2023-07-30 20:17:06 +0700

    handle status dropdown list

--- Admin/app/src/main/java/t3h/android/admin/helper/StatusDropdownHelper.java
+++ Admin/app/src/main/java/t3h/android/admin/helper/StatusDropdownHelper.java
@@ -1,2 +1,15 @@
-package t3h.android.admin.helper;public class StatusDropdownHelper {
+package t3h.android.admin.helper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import t3h.android.admin.model.DropdownItem;
+
+public class StatusDropdownHelper {
+    public static List<DropdownItem> statusDropdown() {
+        List<DropdownItem> itemList = new ArrayList<>();
+        itemList.add(new DropdownItem(0, "Inactive"));
+        itemList.add(new DropdownItem(1, "Active"));
+        return itemList;
+    }
 }
