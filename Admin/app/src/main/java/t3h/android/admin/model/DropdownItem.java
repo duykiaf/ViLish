a87bab0c405d0985658ed3b1e8983a548b12428c
dkifashion <dkiwomanfashion@gmail.com>
2023-07-30 20:15:44 +0700

    build Dropdown item model

--- Admin/app/src/main/java/t3h/android/admin/model/DropdownItem.java
+++ Admin/app/src/main/java/t3h/android/admin/model/DropdownItem.java
@@ -1,2 +1,38 @@
-package t3h.android.admin.model;public class DropdownItem {
+package t3h.android.admin.model;
+
+import androidx.annotation.NonNull;
+
+public class DropdownItem {
+    private int hiddenValue;
+    private String displayText;
+
+    public DropdownItem(int hiddenValue, String displayText) {
+        this.hiddenValue = hiddenValue;
+        this.displayText = displayText;
+    }
+
+    public DropdownItem() {
+    }
+
+    public int getHiddenValue() {
+        return hiddenValue;
+    }
+
+    public void setHiddenValue(int hiddenValue) {
+        this.hiddenValue = hiddenValue;
+    }
+
+    public String getDisplayText() {
+        return displayText;
+    }
+
+    public void setDisplayText(String displayText) {
+        this.displayText = displayText;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return displayText;
+    }
 }
