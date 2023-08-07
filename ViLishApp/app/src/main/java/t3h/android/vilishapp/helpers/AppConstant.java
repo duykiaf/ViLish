3d5d47e8f5b4e71b689323c092d94fda577ffcdd
dkifashion <dkiwomanfashion@gmail.com>
2023-08-07 09:27:57 +0700

    set up app constants

--- ViLishApp/app/src/main/java/t3h/android/vilishapp/helpers/AppConstant.java
+++ ViLishApp/app/src/main/java/t3h/android/vilishapp/helpers/AppConstant.java
@@ -1,2 +1,10 @@
-package t3h.android.vilishapp.helpers;public class AppConstant {
+package t3h.android.vilishapp.helpers;
+
+public class AppConstant {
+    public static final String TOPICS = "Topics";
+    public static final String AUDIOS = "Audios";
+    public static final int TOPICS_LIST_PAGE_SIZE = 10;
+    public static final int AUDIOS_LIST_PAGE_SIZE = 10;
+    public static final String STATUS = "status";
+    public static final int ACTIVE = 1;
 }
