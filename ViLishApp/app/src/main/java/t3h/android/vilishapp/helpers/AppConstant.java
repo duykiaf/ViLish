cf8cb712ab8f7f0a17786bc31057f10c68f129c1
dkifashion <dkiwomanfashion@gmail.com>
2023-08-09 21:22:43 +0700

    add new constant

--- ViLishApp/app/src/main/java/t3h/android/vilishapp/helpers/AppConstant.java
+++ ViLishApp/app/src/main/java/t3h/android/vilishapp/helpers/AppConstant.java
@@ -7,4 +7,5 @@ public class AppConstant {
     public static final int AUDIOS_LIST_PAGE_SIZE = 10;
     public static final String STATUS = "status";
     public static final int ACTIVE = 1;
+    public static final int SPAN_COUNT = 2;
 }
