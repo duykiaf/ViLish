91f3ffea5ea0577f067f22ebe97de6d553caf7d0
dkifashion <dkiwomanfashion@gmail.com>
2023-08-09 21:25:25 +0700

    delete network state code, set layout manager, set adapater and handle menu item click

--- ViLishApp/app/src/main/java/t3h/android/vilishapp/ui/TopicsListFragment.java
+++ ViLishApp/app/src/main/java/t3h/android/vilishapp/ui/TopicsListFragment.java
@@ -1,10 +1,10 @@
 package t3h.android.vilishapp.ui;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,14 +12,15 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 import autodispose2.AutoDispose;
 import autodispose2.androidx.lifecycle.AndroidLifecycleScopeProvider;
 import t3h.android.vilishapp.R;
 import t3h.android.vilishapp.adapters.TopicAdapter;
 import t3h.android.vilishapp.databinding.FragmentTopicsListBinding;
+import t3h.android.vilishapp.helpers.AppConstant;
 import t3h.android.vilishapp.helpers.ItemDiff;
-import t3h.android.vilishapp.helpers.NetworkHelper;
 import t3h.android.vilishapp.viewmodels.TopicViewModel;
 
 public class TopicsListFragment extends Fragment {
@@ -37,17 +38,14 @@ public class TopicsListFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         navController = Navigation.findNavController(requireActivity(), R.id.navHostFragment);
-        if (NetworkHelper.isInternetConnected(requireContext())) {
-            Toast.makeText(requireActivity(), "Internet connected", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(requireActivity(), "Internet disconnected", Toast.LENGTH_SHORT).show();
-        }
         initTopicsList();
     }
 
     private void initTopicsList() {
         TopicViewModel topicViewModel = new ViewModelProvider(requireActivity()).get(TopicViewModel.class);
         TopicAdapter topicAdapter = new TopicAdapter(new ItemDiff<>());
+        binding.topicsRcv.setLayoutManager(new GridLayoutManager(requireActivity(), AppConstant.SPAN_COUNT));
+        binding.topicsRcv.setAdapter(topicAdapter);
         topicViewModel.flowable.to(AutoDispose.autoDisposable(AndroidLifecycleScopeProvider.from(requireActivity())))
                 .subscribe(pagingData -> topicAdapter.submitData(getLifecycle(), pagingData));
 
@@ -56,6 +54,23 @@ public class TopicsListFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
+        onMenuItemClick();
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    private void onMenuItemClick() {
+        binding.appBarFragment.topAppBar.setOnMenuItemClickListener(menu -> {
+            switch (menu.getItemId()) {
+                case R.id.searchItem:
+                    return true;
+                case R.id.bookmarksItem:
+                    return true;
+                case R.id.goToTopItem:
+                    binding.topicsRcv.smoothScrollToPosition(0);
+                    return true;
+            }
+            return false;
+        });
     }
 
     @Override
