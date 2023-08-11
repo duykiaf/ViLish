d45bb9ecc1b65068d9199037a55f9a7444ddfbb5
dkifashion <dkiwomanfashion@gmail.com>
2023-08-11 20:42:06 +0700

    fetch audio list by topic and fetch audio search list

--- ViLishApp/app/src/main/java/t3h/android/vilishapp/ui/AudioListFragment.java
+++ ViLishApp/app/src/main/java/t3h/android/vilishapp/ui/AudioListFragment.java
@@ -1,19 +1,214 @@
 package t3h.android.vilishapp.ui;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import t3h.android.vilishapp.R;
+import t3h.android.vilishapp.adapters.AudioAdapter;
+import t3h.android.vilishapp.databinding.FragmentAudioListBinding;
+import t3h.android.vilishapp.helpers.AppConstant;
+import t3h.android.vilishapp.models.Audio;
 
 public class AudioListFragment extends Fragment {
+    private FragmentAudioListBinding binding;
+    private NavController navController;
+    private FirebaseDatabase firebaseDatabase;
+    private String topicId;
+    private List<Audio> activeAudioList = new ArrayList<>();
+    private List<Audio> audioListByTopicId = new ArrayList<>();
+    private int visibility;
+    private AudioAdapter audioAdapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_audio_list, container, false);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        binding = FragmentAudioListBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(requireActivity(), R.id.navHostFragment);
+        initTopAppBar();
+        topicId = requireArguments().getString(AppConstant.TOPIC_ID);
+        if (topicId != null) {
+            audioAdapter = new AudioAdapter(getContext());
+            binding.audiosRcv.setLayoutManager(new LinearLayoutManager(getContext()));
+            binding.audiosRcv.setAdapter(audioAdapter);
+            binding.progressBar.setVisibility(View.VISIBLE);
+            initAudioListByTopicId();
+        } else {
+            Toast.makeText(requireActivity(), AppConstant.SYSTEM_ERROR, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void initTopAppBar() {
+        binding.appBarFragment.topAppBar.setTitle(AppConstant.AUDIO_SCREEN_TITLE);
+        binding.appBarFragment.topAppBar.setNavigationIcon(R.drawable.arrow_back_ic);
+    }
+
+    private void initAudioListByTopicId() {
+        DatabaseReference databaseReference = firebaseDatabase.getReference().child(AppConstant.AUDIOS);
+        databaseReference.keepSynced(true);
+        Query baseQuery = databaseReference.orderByChild(AppConstant.STATUS).equalTo(AppConstant.ACTIVE);
+        Query finalQuery = databaseReference.orderByChild(AppConstant.TOPIC_ID).equalTo(topicId);
+        baseQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                activeAudioList.clear();
+                for (DataSnapshot data: snapshot.getChildren()) {
+                    Audio audio = data.getValue(Audio.class);
+                    if (audio != null) {
+                        activeAudioList.add(audio);
+                    }
+                }
+                if (activeAudioList != null && !activeAudioList.isEmpty()) {
+                    finalQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            audioListByTopicId.clear();
+                            for (DataSnapshot data: snapshot.getChildren()) {
+                                Audio audio = data.getValue(Audio.class);
+                                if (audio != null && activeAudioList.contains(audio)) {
+                                    audioListByTopicId.add(audio);
+                                }
+                            }
+                            if (!audioListByTopicId.isEmpty()) {
+                                visibility = View.GONE;
+                            } else {
+                                visibility = View.VISIBLE;
+                            }
+                            binding.noDataTxt.setVisibility(visibility);
+                            audioAdapter.updateItemList(audioListByTopicId);
+                            binding.progressBar.setVisibility(View.GONE);
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                            binding.progressBar.setVisibility(View.GONE);
+                            Toast.makeText(requireActivity(), AppConstant.SYSTEM_ERROR, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } else {
+                    binding.progressBar.setVisibility(View.GONE);
+                    binding.noDataTxt.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                binding.progressBar.setVisibility(View.GONE);
+                Toast.makeText(requireActivity(), AppConstant.SYSTEM_ERROR, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        binding.appBarFragment.topAppBar.setOnMenuItemClickListener(this::onMenuItemClick);
+        binding.appBarFragment.topAppBar.setNavigationOnClickListener(v -> requireActivity().onBackPressed());
+        binding.closeSearchLayout.setOnClickListener(v -> {
+            binding.searchEdtLayout.setVisibility(View.GONE);
+            binding.closeSearchLayout.setVisibility(View.GONE);
+            reloadAudioListAfterSearch();
+        });
+    }
+
+    private boolean onMenuItemClick(MenuItem menu) {
+        switch (menu.getItemId()) {
+            case R.id.searchItem:
+                if (binding.searchEdtLayout.getVisibility() == View.VISIBLE) {
+                    visibility = View.GONE;
+                    reloadAudioListAfterSearch();
+                } else {
+                    visibility = View.VISIBLE;
+                    binding.searchEdt.requestFocus();
+                    searchEdtTextChanged();
+                }
+                binding.searchEdtLayout.setVisibility(visibility);
+                binding.closeSearchLayout.setVisibility(visibility);
+                return true;
+            case R.id.bookmarksItem:
+                return true;
+            case R.id.goToTopItem:
+                binding.audiosRcv.smoothScrollToPosition(0);
+                return true;
+        }
+        return false;
+    }
+    private void reloadAudioListAfterSearch() {
+        binding.searchEdt.setText("");
+        initAudioListByTopicId();
+    }
+
+    private void searchEdtTextChanged() {
+        binding.searchEdt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                getAudioSearchList(editable.toString().trim());
+            }
+        });
+    }
+
+    private void getAudioSearchList(String keyword) {
+        List<Audio> audioSearchList = new ArrayList<>();
+        for (Audio audio: audioListByTopicId) {
+            if (audio.getName().toLowerCase().contains(keyword.toLowerCase())) {
+                audioSearchList.add(audio);
+            }
+        }
+        if (audioSearchList.isEmpty()) {
+            visibility = View.VISIBLE;
+        } else {
+            visibility = View.GONE;
+        }
+        binding.noDataTxt.setVisibility(visibility);
+        audioAdapter.searchList(audioSearchList);
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 }
