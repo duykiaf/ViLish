d90774e5c87bfe0c70ac90776f14c0e5385d3d20
dkifashion <dkiwomanfashion@gmail.com>
2023-08-11 20:41:01 +0700

    enable firebase database persistence

--- ViLishApp/app/src/main/java/t3h/android/vilishapp/MainActivity.java
+++ ViLishApp/app/src/main/java/t3h/android/vilishapp/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseDatabase firebaseDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseDatabase.setPersistenceEnabled(true);
         setContentView(R.layout.activity_main);
     }
 }
