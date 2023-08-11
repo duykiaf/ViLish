87df0165d5ce2845e8feed6371a005fbc7312a59
dkifashion <dkiwomanfashion@gmail.com>
2023-08-11 20:41:24 +0700

    build audio adapter class

--- ViLishApp/app/src/main/java/t3h/android/vilishapp/adapters/AudioAdapter.java
+++ ViLishApp/app/src/main/java/t3h/android/vilishapp/adapters/AudioAdapter.java
@@ -1,2 +1,94 @@
-package t3h.android.vilishapp.adapters;public class AudioAdapter {
+package t3h.android.vilishapp.adapters;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import t3h.android.vilishapp.databinding.AudioItemLayoutBinding;
+import t3h.android.vilishapp.helpers.ItemDiff;
+import t3h.android.vilishapp.models.Audio;
+
+public class AudioAdapter extends RecyclerView.Adapter<AudioAdapter.AudioViewHolder> {
+    private Context context;
+    private List<Audio> itemList, dataSource;
+    private OnAudioItemClickListener onAudioItemClickListener;
+    private DiffUtil.DiffResult diffResult;
+
+    public AudioAdapter(Context context) {
+        this.context = context;
+        itemList = new ArrayList<>();
+        dataSource = new ArrayList<>();
+    }
+
+    public void setOnAudioItemClickListener(OnAudioItemClickListener listener) {
+        onAudioItemClickListener = listener;
+    }
+
+    @NonNull
+    @Override
+    public AudioViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new AudioViewHolder(AudioItemLayoutBinding.inflate(
+                LayoutInflater.from(parent.getContext()), parent, false
+        ));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AudioViewHolder holder, int position) {
+        Log.e("DNV", String.valueOf(position));
+        holder.bindView(itemList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList == null ? 0 : itemList.size();
+    }
+
+    public class AudioViewHolder extends RecyclerView.ViewHolder {
+        private AudioItemLayoutBinding binding;
+
+        public AudioViewHolder(AudioItemLayoutBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+
+            binding.audioItemLayout.setOnClickListener(v -> {
+                if (onAudioItemClickListener != null) {
+                    onAudioItemClickListener.onItemClick(itemList.get(getAdapterPosition()));
+                }
+            });
+        }
+
+        public void bindView(Audio audioInfo) {
+            binding.audioTitle.setText(audioInfo.getName());
+        }
+    }
+
+    public void updateItemList(List<Audio> newItemList) {
+        diffResult = DiffUtil.calculateDiff(new ItemDiff<>(newItemList, itemList));
+        itemList.clear();
+        itemList.addAll(newItemList);
+        dataSource.clear();
+        dataSource.addAll(newItemList);
+        diffResult.dispatchUpdatesTo(this);
+    }
+
+    public void searchList(List<Audio> searchList) {
+        if (dataSource != null) {
+            diffResult = DiffUtil.calculateDiff(new ItemDiff<>(searchList, itemList));
+            itemList.clear();
+            itemList.addAll(searchList);
+            diffResult.dispatchUpdatesTo(this);
+        }
+    }
+
+    public interface OnAudioItemClickListener {
+        void onItemClick(Audio item);
+    }
 }
