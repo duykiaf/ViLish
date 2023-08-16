f24220323f54a572281e2572a9a124fa34902403
dkifashion <dkiwomanfashion@gmail.com>
2023-08-16 10:41:48 +0700

    add new constants

--- ViLishApp/app/src/main/java/t3h/android/vilishapp/helpers/AppConstant.java
+++ ViLishApp/app/src/main/java/t3h/android/vilishapp/helpers/AppConstant.java
@@ -11,4 +11,14 @@ public class AppConstant {
     public static final String SYSTEM_ERROR = "System error. Please try again";
     public static final String TOPIC_ID = "topicId";
     public static final String AUDIO_SCREEN_TITLE = "Audio";
+    public static final String AUDIO_ID = "audioId";
+    public static final String AUDIO_TRANSLATIONS = "translations";
+    public static final String LYRICS = "Lyrics";
+    public static final String TRANSLATIONS = "Translations";
+    public static final String TOPIC_NAME = "topicName";
+    public static final String DURATION_DEFAULT = "0:00";
+    public static final String PLAY_ICON = "Play icon";
+    public static final String PAUSE_ICON = "Pause icon";
+    public static final String BOOKMARK_BORDER_ICON = "Bookmark border icon";
+    public static final String BOOKMARK_ICON = "Bookmark icon";
 }
