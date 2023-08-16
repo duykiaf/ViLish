21d63edda96baa14681e3e7ee483f2468ff1a7ff
dkifashion <dkiwomanfashion@gmail.com>
2023-08-16 10:42:55 +0700

    build adapter to init viewpager

--- ViLishApp/app/src/main/java/t3h/android/vilishapp/adapters/FragmentAdapter.java
+++ ViLishApp/app/src/main/java/t3h/android/vilishapp/adapters/FragmentAdapter.java
@@ -1,2 +1,28 @@
-package t3h.android.vilishapp.adapters;public class FragmentAdapter {
+package t3h.android.vilishapp.adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.List;
+
+public class FragmentAdapter extends FragmentStateAdapter {
+    private List<Fragment> fragments;
+
+    public FragmentAdapter(@NonNull FragmentActivity fragmentActivity, List<Fragment> fragments) {
+        super(fragmentActivity);
+        this.fragments = fragments;
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return fragments.size();
+    }
 }
