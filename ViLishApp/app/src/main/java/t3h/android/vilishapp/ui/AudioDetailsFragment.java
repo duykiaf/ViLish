d58d07c49d761476da966c79a178c34ae6fa5c8c
dkifashion <dkiwomanfashion@gmail.com>
2023-08-16 10:47:16 +0700

    assign FragmentAudioDetailsBinding but do not use

--- ViLishApp/app/src/main/java/t3h/android/vilishapp/ui/AudioDetailsFragment.java
+++ ViLishApp/app/src/main/java/t3h/android/vilishapp/ui/AudioDetailsFragment.java
@@ -1,20 +1,44 @@
 package t3h.android.vilishapp.ui;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import t3h.android.vilishapp.R;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import t3h.android.vilishapp.databinding.FragmentAudioDetailsBinding;
 
 public class AudioDetailsFragment extends Fragment {
+    private FragmentAudioDetailsBinding binding;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_audio_details, container, false);
+        binding = FragmentAudioDetailsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
     }
 }
