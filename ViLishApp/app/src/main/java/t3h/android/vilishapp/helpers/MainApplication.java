3ec7e854d997b053c1beb4338186c7cc6fbe157b
dkifashion <dkiwomanfashion@gmail.com>
2023-08-27 21:42:51 +0700

    build notification channel, but do not use

--- ViLishApp/app/src/main/java/t3h/android/vilishapp/helpers/MainApplication.java
+++ ViLishApp/app/src/main/java/t3h/android/vilishapp/helpers/MainApplication.java
@@ -1,2 +1,27 @@
-package t3h.android.vilishapp.helpers;public class MainApplication {
+package t3h.android.vilishapp.helpers;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(
+                    AppConstant.CHANNEL_ID, AppConstant.CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH
+            );
+            notificationChannel.setSound(null, null);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            if (manager != null) {
+                manager.createNotificationChannel(notificationChannel);
+            }
+        }
+    }
 }
