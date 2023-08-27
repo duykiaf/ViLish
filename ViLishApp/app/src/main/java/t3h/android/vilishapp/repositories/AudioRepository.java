c69264f41e0801bc6f016213318985fa5e717046
dkifashion <dkiwomanfashion@gmail.com>
2023-08-27 21:51:19 +0700

    build repository class to access local database

--- ViLishApp/app/src/main/java/t3h/android/vilishapp/repositories/AudioRepository.java
+++ ViLishApp/app/src/main/java/t3h/android/vilishapp/repositories/AudioRepository.java
@@ -1,2 +1,41 @@
-package t3h.android.vilishapp.repositories;public class AudioRepository {
+package t3h.android.vilishapp.repositories;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import t3h.android.vilishapp.dao.BookmarksDao;
+import t3h.android.vilishapp.database.LocalDatabase;
+import t3h.android.vilishapp.models.Audio;
+
+public class AudioRepository {
+    private BookmarksDao bookmarksDao;
+
+    public AudioRepository() {
+    }
+
+    public AudioRepository(Application application) {
+        bookmarksDao = LocalDatabase.getInstance(application).bookmarksDao();
+    }
+
+    public LiveData<List<Audio>> getBookmarkList() {
+        return bookmarksDao.getAllList();
+    }
+
+    public LiveData<List<String>> getBookmarkAudioIds() {
+        return bookmarksDao.getBookmarkAudioIds();
+    }
+
+    public void addBookmark(Audio audio) {
+        bookmarksDao.addBookmark(audio);
+    }
+
+    public int deleteBookmark(Audio audio) {
+        if (audio != null) {
+            return bookmarksDao.deleteBookmark(audio);
+        }
+        return 0;
+    }
 }
