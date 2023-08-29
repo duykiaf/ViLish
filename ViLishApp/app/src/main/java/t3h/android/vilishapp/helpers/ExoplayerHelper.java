32b194398ebe9c84f8a16607edd7b1b2ce316689
dkifashion <dkiwomanfashion@gmail.com>
2023-08-29 21:03:51 +0700

    comment playerListener, showPlayOrPauseIcon, initSeekBarAudioDuration, updatePlayerPositionProgress methods

--- ViLishApp/app/src/main/java/t3h/android/vilishapp/helpers/ExoplayerHelper.java
+++ ViLishApp/app/src/main/java/t3h/android/vilishapp/helpers/ExoplayerHelper.java
@@ -43,80 +43,80 @@ public class ExoplayerHelper {
                 .build();
     }
 
-    public static void playerListener(ExoPlayer player, FragmentAudioListBinding binding, AudioViewModel audioViewModel) {
-        player.addListener(new Player.Listener() {
-            @Override
-            public void onMediaItemTransition(@Nullable MediaItem mediaItem, int reason) {
-                // show the playing audio title
-                assert mediaItem != null;
-                binding.currentAudioTitle.setText(mediaItem.mediaMetadata.title);
-                binding.fragmentAudioDetailsLayout.audioTitle.setText(mediaItem.mediaMetadata.title);
-
-                // show audio lyrics and translations
-                audioViewModel.setAudioLyrics((String) mediaItem.mediaMetadata.description);
-                audioViewModel.setAudioTranslations((String) mediaItem.mediaMetadata.extras.get(AppConstant.AUDIO_TRANSLATIONS));
-
-                // show pause icon
-                showPlayOrPauseIcon(true, binding);
-
-                // init seek bar audio duration (AudioDetailsFragment)
-                initSeekBarAudioDuration(player, binding);
-
-                if (!player.isPlaying()) {
-                    player.play();
-                }
-            }
-
-            @Override
-            public void onPlaybackStateChanged(int playbackState) {
-                if (playbackState == ExoPlayer.STATE_READY && player.isPlaying()) {
-                    // show the playing audio title
-                    binding.currentAudioTitle.setText(Objects.requireNonNull(player.getCurrentMediaItem()).mediaMetadata.title);
-                    binding.fragmentAudioDetailsLayout.audioTitle.setText(Objects.requireNonNull(player.getCurrentMediaItem()).mediaMetadata.title);
-
-                    // show audio lyrics and translations
-                    audioViewModel.setAudioLyrics((String) player.getCurrentMediaItem().mediaMetadata.description);
-                    audioViewModel.setAudioTranslations((String) player.getCurrentMediaItem().mediaMetadata.extras.get(AppConstant.AUDIO_TRANSLATIONS));
-
-                    // show pause audio icon
-                    showPlayOrPauseIcon(true, binding);
-
-                    // init seek bar audio duration (AudioDetailsFragment)
-                    initSeekBarAudioDuration(player, binding);
-                } else {
-                    // show play audio icon
-                    showPlayOrPauseIcon(false, binding);
-                }
-            }
-        });
-    }
-
-    private static void showPlayOrPauseIcon(boolean isPlaying, FragmentAudioListBinding binding) {
-        int resId;
-        if (isPlaying) {
-            resId = R.drawable.pause_circle_blue_outline_ic;
-        } else {
-            resId = R.drawable.play_circle_blue_outline_ic;
-        }
-        binding.playOrPauseIcon.setImageResource(resId);
-        binding.fragmentAudioDetailsLayout.playOrPauseIcon.setImageResource(resId);
-    }
-
-    private static void initSeekBarAudioDuration(ExoPlayer player, FragmentAudioListBinding binding) {
-        binding.fragmentAudioDetailsLayout.audioDuration.setText(AudioHelper.milliSecondsToTimer((int) player.getDuration()));
-        binding.fragmentAudioDetailsLayout.seekBar.setMax((int) player.getDuration());
-        binding.fragmentAudioDetailsLayout.seekBar.setProgress((int) player.getCurrentPosition());
-        binding.fragmentAudioDetailsLayout.audioCurrentTime.setText(AudioHelper.milliSecondsToTimer((int) player.getCurrentPosition()));
-        updatePlayerPositionProgress(player, binding);
-    }
-
-    private static void updatePlayerPositionProgress(ExoPlayer player, FragmentAudioListBinding binding) {
-        new Handler().postDelayed(() -> {
-            if (player.isPlaying() && binding != null) {
-                binding.fragmentAudioDetailsLayout.audioCurrentTime.setText(AudioHelper.milliSecondsToTimer((int) player.getCurrentPosition()));
-                binding.fragmentAudioDetailsLayout.seekBar.setProgress((int) player.getCurrentPosition());
-            }
-            updatePlayerPositionProgress(player, binding);
-        }, 1000);
-    }
+//    public static void playerListener(ExoPlayer player, FragmentAudioListBinding binding, AudioViewModel audioViewModel) {
+//        player.addListener(new Player.Listener() {
+//            @Override
+//            public void onMediaItemTransition(@Nullable MediaItem mediaItem, int reason) {
+//                // show the playing audio title
+//                assert mediaItem != null;
+//                binding.currentAudioTitle.setText(mediaItem.mediaMetadata.title);
+//                binding.fragmentAudioDetailsLayout.audioTitle.setText(mediaItem.mediaMetadata.title);
+//
+//                // show audio lyrics and translations
+//                audioViewModel.setAudioLyrics((String) mediaItem.mediaMetadata.description);
+//                audioViewModel.setAudioTranslations((String) mediaItem.mediaMetadata.extras.get(AppConstant.AUDIO_TRANSLATIONS));
+//
+//                // show pause icon
+//                showPlayOrPauseIcon(true, binding);
+//
+//                // init seek bar audio duration (AudioDetailsFragment)
+//                initSeekBarAudioDuration(player, binding);
+//
+//                if (!player.isPlaying()) {
+//                    player.play();
+//                }
+//            }
+//
+//            @Override
+//            public void onPlaybackStateChanged(int playbackState) {
+//                if (playbackState == ExoPlayer.STATE_READY && player.isPlaying()) {
+//                    // show the playing audio title
+//                    binding.currentAudioTitle.setText(Objects.requireNonNull(player.getCurrentMediaItem()).mediaMetadata.title);
+//                    binding.fragmentAudioDetailsLayout.audioTitle.setText(Objects.requireNonNull(player.getCurrentMediaItem()).mediaMetadata.title);
+//
+//                    // show audio lyrics and translations
+//                    audioViewModel.setAudioLyrics((String) player.getCurrentMediaItem().mediaMetadata.description);
+//                    audioViewModel.setAudioTranslations((String) player.getCurrentMediaItem().mediaMetadata.extras.get(AppConstant.AUDIO_TRANSLATIONS));
+//
+//                    // show pause audio icon
+//                    showPlayOrPauseIcon(true, binding);
+//
+//                    // init seek bar audio duration (AudioDetailsFragment)
+//                    initSeekBarAudioDuration(player, binding);
+//                } else {
+//                    // show play audio icon
+//                    showPlayOrPauseIcon(false, binding);
+//                }
+//            }
+//        });
+//    }
+//
+//    private static void showPlayOrPauseIcon(boolean isPlaying, FragmentAudioListBinding binding) {
+//        int resId;
+//        if (isPlaying) {
+//            resId = R.drawable.pause_circle_blue_outline_ic;
+//        } else {
+//            resId = R.drawable.play_circle_blue_outline_ic;
+//        }
+//        binding.playOrPauseIcon.setImageResource(resId);
+//        binding.fragmentAudioDetailsLayout.playOrPauseIcon.setImageResource(resId);
+//    }
+//
+//    private static void initSeekBarAudioDuration(ExoPlayer player, FragmentAudioListBinding binding) {
+//        binding.fragmentAudioDetailsLayout.audioDuration.setText(AudioHelper.milliSecondsToTimer((int) player.getDuration()));
+//        binding.fragmentAudioDetailsLayout.seekBar.setMax((int) player.getDuration());
+//        binding.fragmentAudioDetailsLayout.seekBar.setProgress((int) player.getCurrentPosition());
+//        binding.fragmentAudioDetailsLayout.audioCurrentTime.setText(AudioHelper.milliSecondsToTimer((int) player.getCurrentPosition()));
+//        updatePlayerPositionProgress(player, binding);
+//    }
+//
+//    private static void updatePlayerPositionProgress(ExoPlayer player, FragmentAudioListBinding binding) {
+//        new Handler().postDelayed(() -> {
+//            if (player.isPlaying() && binding != null) {
+//                binding.fragmentAudioDetailsLayout.audioCurrentTime.setText(AudioHelper.milliSecondsToTimer((int) player.getCurrentPosition()));
+//                binding.fragmentAudioDetailsLayout.seekBar.setProgress((int) player.getCurrentPosition());
+//            }
+//            updatePlayerPositionProgress(player, binding);
+//        }, 1000);
+//    }
 }
