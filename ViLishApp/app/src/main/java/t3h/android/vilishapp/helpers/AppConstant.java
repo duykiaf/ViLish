7b65c85bfad25e108bcba0d6fe9c44ac1c1f73c4
dkifashion <dkiwomanfashion@gmail.com>
2023-09-20 15:46:04 +0700

    remove 2 constants and add new constant

--- ViLishApp/app/src/main/java/t3h/android/vilishapp/helpers/AppConstant.java
+++ ViLishApp/app/src/main/java/t3h/android/vilishapp/helpers/AppConstant.java
@@ -9,8 +9,6 @@ public class AppConstant {
     public static final String SYSTEM_ERROR = "System error. Please try again";
     public static final String TOPIC_ID = "topicId";
     public static final String AUDIO_SCREEN_TITLE = "Audio";
-    public static final String AUDIO_ID = "audioId";
-    public static final String AUDIO_TRANSLATIONS = "translations";
     public static final String LYRICS = "Lyrics";
     public static final String TRANSLATIONS = "Translations";
     public static final String TOPIC_NAME = "topicName";
@@ -42,6 +40,7 @@ public class AppConstant {
     public static final int MAX_DOWNLOAD_FILES = 10;
     public static final String PLAY_ANOTHER_AUDIO = "playAnotherAudio";
     public static final String CURRENT_MEDIA_ITEM_INDEX = "currentMediaItemIndex";
+    public static final String AUDIO_ITEM = "audioItem";
     public static int REQUEST_CODE = 6798;
     public static final String BOOKMARK_ICON = "Bookmark icon";
     public static final String BOOKMARK_BORDER_ICON = "Bookmark border icon";
