fad39e1c71453e0db733c85091cfe89409c9d9c1
dkifashion <dkiwomanfashion@gmail.com>
2023-10-05 20:12:56 +0700

    check wifi available by sdk version

--- ViLishApp/app/src/main/java/t3h/android/vilishapp/helpers/NetworkHelper.java
+++ ViLishApp/app/src/main/java/t3h/android/vilishapp/helpers/NetworkHelper.java
@@ -2,15 +2,29 @@ package t3h.android.vilishapp.helpers;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
+import android.os.Build;
 
 public class NetworkHelper {
     public static boolean isInternetConnected(Context context) {
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (connectivityManager != null) {
-            NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-            return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+        if (connectivityManager == null) {
+            return false;
         }
-        return false;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Network network = connectivityManager.getActiveNetwork();
+            if (network == null) {
+                return false;
+            }
+
+            NetworkCapabilities capabilities = connectivityManager.getNetworkCapabilities(network);
+            return capabilities != null && capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI);
+        } else {
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            return networkInfo != null && networkInfo.isConnected();
+        }
     }
 }
