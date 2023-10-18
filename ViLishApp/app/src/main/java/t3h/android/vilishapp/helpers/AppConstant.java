7f3683cd7387d58ddb2d0da48aba49608944f7c3
dkifashion <dkiwomanfashion@gmail.com>
2023-10-18 15:15:51 +0700

    add new constant

--- ViLishApp/app/src/main/java/t3h/android/vilishapp/helpers/AppConstant.java
+++ ViLishApp/app/src/main/java/t3h/android/vilishapp/helpers/AppConstant.java
@@ -52,6 +52,7 @@ public class AppConstant {
     public static final String DOWNLOAD_IS_IN_PROGRESS = "The download is in progress. Please wait";
     public static final String DELETE_SUCCESS = "Delete successfully";
     public static final String DELETE_FAILED = "Delete failed";
+    public static final String AUDIO_LIST_BY_ID = "Audio list";
     public static int REQUEST_CODE = 6798;
     public static final String BOOKMARK_ICON = "Bookmark icon";
     public static final String BOOKMARK_BORDER_ICON = "Bookmark border icon";
